4eb49c4064c163df4b1775ce13647e421b2ba303
Timox <40746064+1017756788@users.noreply.github.com>
2018-07-10 15:21:06 +0800

    重写

--- Timox_vector.hpp
+++ Timox_vector.hpp
@@ -1,291 +1,303 @@
-#include<stdio.h>
-#include<stdlib.h>
-#ifndef VECTOR_Timox
-#define VECTOR_Timox
+#include<cstdio>
+#include<cstdlib>
+#ifndef Timox_vector
+#define Timox_vector
 template<typename T>
 class vector
 {
 public:
-    using iterator=T*;
-    using size_type=size_t;
-    using value_type=T;
-    using difference_type=ptrdiff_t;
-    using reference=T&;
-    using const_reference=const T&;
-    using pointer=T*;
-    using const_pointer=const T*;
-    using const_iterator=const T*;
     class reverse_iterator
     {
     private:
-        pointer t;
+        T* t;
     public:
-        reverse_iterator(pointer W = nullptr) : t(W) {}
-        value_type operator *(){    return *t;}
-        bool operator !=(const reverse_iterator & it) const{    return t != it.t;}
-        bool operator ==(const reverse_iterator & it) const{    return t == it.t;}
-        reverse_iterator operator +=(size_type it){    return t-=it;}
-        reverse_iterator operator -=(size_type it){    return t+=it;}
-        reverse_iterator operator +(size_type it){    return t-it;}
-        reverse_iterator operator -(size_type it){    return t+it;}
-        reverse_iterator operator ++(){    return --t;}
-        reverse_iterator operator --(){    return ++t;}
-        void operator ++(int)
-        {
-            pointer tmp = t;
-            --t;
-            return tmp;
+        reverse_iterator (T* a = nullptr) :t(a) {}
+        bool operator != (const reverse_iterator & b){
+            return t != b.t;
         }
-        void operator --(int)
-        {
-            pointer tmp = t;
-            ++t;
-            return tmp;
+        bool operator == (const reverse_iterator * b){
+            return t == b.t;
+        }
+        T operator* ( ){
+            return t*;
+        }
+        T* operator ++(){
+            return --t;
+        }
+        T* operator--(){
+            return ++t;
+        }
+        T* operator++(int){
+            T* c = t;
+            t --;
+            return c;
+        }
+        T* operator--(int){
+            T* c = t;
+            t ++;
+            return c;
+        }
+        T* operator+(size_t b){
+            return t - b;
+        }
+        T* operator-(size_t b){
+            return t + b;
+        }
+        T* operator+=(size_t b){
+            return t -= b;
+        }
+        T* operator-=(size_t b){
+            return t += b;
         }
     };
 private:
-    size_type length;
-    size_type capacity_;
-    pointer data_;
+    size_t length;
+    size_t _capacity;
+    T * _data;
 public:
-    size_type size(){    return length; }
-    size_type capacity(){    return capacity_;}
-    bool empty(){    return length==0;}
-    value_type operator [] (size_type pos){return data_[pos];}
-    vector & operator = (vector tmp)
+    vector() 
+    :length(0) 
+    , _capacity(1) 
     {
-        vector n;
-        for(auto i=tmp.begin();i!=tmp.end();i++)
-            n.push_back(tmp);
-        return n;
+        _data = new T [_capacity];
     }
-    vector()
+
+    explicit vector (T n)
+    :length(n)
+    ,_capacity(n * 2)
     {
-        length=0;
-        capacity_=1;
-        data_=new value_type[capacity_];
+        _data = new T [_capacity];
     }
-    explicit vector(size_type nums)
+
+    vector(T n,T a)
+    :length(n)
+    ,capacity(n * 2)
     {
-        length=nums;
-        capacity_=2*nums;
-        data_=new value_type[capacity_];
+        _data = new T [_capacity];
+        for(auto i = 0; i < n; i ++)
+            _data[i] = a;
     }
-    vector(size_type nums,value_type t)
+
+    vector(const vector & a)
+    :length(a.length)
+    ,_capacity(a.capacity)
     {
-        length=nums;
-        capacity_=2*nums;
-        data_=new value_type[capacity_];
-        for(size_type i=0;i<nums;i++)
-            data_[i]=t;
+        for(auto i = 0; i < length; i ++)
+            _data[i] = a._data[i];
     }
-    vector(const vector &v)
-    {
-        length=v.length;
-        capacity_=v.capacity_;
-        data_=new value_type[capacity_];
-        for(size_type i=0;i<capacity_;i++)
-            data_[i]=v.data_[i];
-    }
-    vector(iterator a,iterator b)
-    {
-        length=0;
-        capacity_=1;
-        data_=new value_type[capacity_];
-        while(a!=b)
-        {
-            push_back(a);
-            a++;
-        }
-    }
+
     ~vector()
     {
-        delete[] data_;
+        delete [] _data;
     }
-    void reserve(size_type num)
-    {
-        if(num<=lenth)
-            return ;
-        size_type r=capacity_;
-        capacity_=num;
-        pointer fr=data_;
-        pointer ndata_=new value_type[capacity_];
-        for(size_type i=0;i<r;i++)
-            ndata_[i]=data_[i];
-        data_=ndata_;
-        delete fr;
-    }
-    void resize(size_type num)
-    {
-        if(num<=length)
-        {
-            length=num;
-            reserve(2*num);
-            return;
-        }
-        reserve(2*num);
-        length=num;
-    }
-    void resize(size_type num,value_type value)
-    {
-        if(num<=length)
-        {
-            length=num;
-            reserve(2*num);
-            return;
-        }
-        reserve(2*num);
-        for(size_type i=length;i<num;i++)
-            data_[i]=value;
-        length=num;
-    }
-    reference front(){   return *begin();}
-    reference back()
-    {
-        iterator endIt = end();
-        return *(--endIt);
-    }
-    const_reference back() const{    return *(--end());}
-    reference at(size_type pos)
-    {
-        if(pos<0||pos>length)
-            exit(1);
-        return data_[pos];
-    }
-    iterator begin(){    return data_;}
-    const_iterator begin() const{    return data_;}
-    const_iterator cbegin() const noexcept{    return data_;}
 
-    iterator end(){    return data_+length;}
-    const_iterator end() const{    return data_+length;}
-    const_iterator cend() const noexcept{    return data_+length;}
+	vector & operator =(const vector& b)//???
+	{
+	    if(*this != b)
+	    {
+	    	T* r = _data;
+	    	theSize = b.theSize;
+	    	theCapacity = b.theCapacity;
+	    	T * ndata = new T [theCapacity];
+	    	for(auto i = b.begin(); i < b.end(); i++)
+	    	    ndata[i]	= b._data[i];
+            _data = ndata;
+            delete r;
+	    } 
+	    return *this;
+	}
 
-    reverse_iterator rbegin(){    return data_+length-1;}
-    const reverse_iterator rbegin() const{    return data_+length-1;}
-    const reverse_iterator crbegin() const noexcept{    return data_+length-1;}
+    T & at( size_t pos )
+    {
+        if(pos < 0 || pos > length)
+            exit(1);
+        return _data[pos];
+    }
+    const T & at( size_t pos ) const
+    {
+        if(pos < 0 || pos > length)
+            exit(1);
+        return _data[pos];
+    }
 
-    reverse_iterator rend(){    return data_-1;}
-    const reverse_iterator rend() const{    return data_-1;}
-    const reverse_iterator crend() const noexcept{    return data_-1;}
+    T & front(){    return *begin();}
+    const T & front()const{    return *begin();}
 
-    void push_back(const iterator A)
-    {
-        data_[length++] = *A;
-        if(length == capacity_)
-        {
-            capacity_ *= 2;
-            pointer fr = data_;
-            pointer data_2 = new value_type[capacity_];
-            for(size_type i = 0; i < capacity_ / 2; i ++)
-                data_2[i]=data_[i];
-            data_=data_2;
-            delete fr;
-        }
-    }   
-    void push_back(const value_type a)
-    {
-        data_[length++]=a;
-        if(length==capacity_)
-        {
-            capacity_*=2;
-            pointer fr=data_;
-            pointer data_2=new value_type[capacity_];
-            for(size_type i=0;i<capacity_/2;i++)
-                data_2[i]=data_[i];
-            data_=data_2;
-            delete fr;
-        }
+    T & back(){     return --(*end());}
+    const T & back()const{     return --(*end());}
+
+    T* data() noexcept{     return _data;}
+    const T* data() const noexcept{     return _data;}
+
+    T operator [](const size_t x){
+        return _data[x];
     }
-    void pop_back()
-    {
-        if(length==0)
-            exit(1);
-        length--;
+
+    T * begin(){    return _data;}
+    T * begin() noexcept{    return _data;}
+    const T * begin() const{    return _data;}
+    const T * cbegin() const noexcept{    return _data;}
+
+    T * end(){  return _data + length;}
+    T * end() noexcept{  return _data + length;}
+    const T * end() const{  return _data + length;}
+    const T * cend() const noexcept{  return _data + length;}
+
+    reverse_iterator rbegin(){   return _data + length - 1;}
+    reverse_iterator rbegin() noexcept{   return _data + length - 1;}
+    const reverse_iterator rbegin() const{   return _data + length - 1;}
+    const reverse_iterator crbegin() const noexcept{   return _data + length - 1;}
+
+    reverse_iterator rend();
+    reverse_iterator rend() noexcept;
+    const reverse_iterator rend() const;
+    const reverse_iterator crend() const noexcept;
+
+    size_t size(){
+        return length;
     }
-    iterator erase(iterator iter)
-    {
-        if(length==0)
-            exit(1);
-        length--;
-        for(auto i=iter;i!=end();i++)
-            *i=*(i+1);
-        return iter;
+
+    size_t capacity(){
+        return _capacity;
     }
-    iterator erase(size_type pos)
-    {
-        if(length==0)
-            exit(1);
-        length--;
-        for(size_type i=pos;i<length;i++)
-            data_[i]=data_[i+1];
-        return begin()+pos;
+
+    bool empty(){
+        return length == 0;
     }
-    void swap(vector & s)
-    {
-        pointer tmpdata_=data_;
-        data_=s.data_;
-        s.data_=tmpdata_;
-        size_type tmplength=length;
-        length=s.length;
-        s.length=tmplength;
-        size_type tmpcapacity_=capacity_;
-        capacity_=s.capacity_;
-        s.capacity_=tmpcapacity_;
+
+    size_t max_size() const
+    {     
+        return _capacity;
     }
-    void assign(size_type num,const_reference value )
+
+    size_t max_size() const noexcept
+    {     
+        return _capacity;
+    }
+
+    void reserve( size_t new_cap )
     {
-        if(length<num)
-        {
-            size_type i=0;
-            for(;i<length;i++)
-                data_[i]=value;
-            for(;i<num;i++)
-                push_back(value);
-        }else
-        {
-            length=num;
-            for(size_type i=0;i<length;i++)
-                data_[i]=value;
+        if(new_cap > length){
+            size_t a = length;
+            capacity = new_cap;
+            T * r = _data;
+            T * ndata = new T [capacity];
+            for(auto i = 0; i < a; i)
+                ndata[i] = _data[i];
+            _data = ndata;
+            delete [] r; 
         }
     }
-    pointer data()
-    {
-        return data_;
-    }
+
     void shrink_to_fit()
     {
-        capacity_=length+1;
-        pointer tmp=data_;
-        pointer ndata_=new value_type[capacity_];
-        for(size_type i=0;i<length;i++)
-            ndata_[i]=data_[i];
-        data_=ndata_;
-        delete[] tmp;
+            T * r = _data;
+            _capacity = length;
+            T * ndata = new T [_capacity];
+            for(auto i = 0; i < length; i ++)
+                ndata[i] = _data[i];
+            _data = ndata;
+            delete [] r; 
     }
+
     void clear()
     {
-        while(length)
-        {
-            pop_back();
-        }
+        length = 0;
     }
-    iterator insert(iterator pos,const_reference value)
+
+    void clear() noexcept
+    {
+        length = 0;
+    }
+
+    T * insert( T * pos,const T & n)
+    {
+        length ++;
+        if(length > capacity){
+            T * r - _data;
+            capacity = capacity * 2;
+            T * ndata = new T [capacity];
+            for(auto i = 0; i < length; i ++)
+                ndata[i] = _data[i];
+            _data = ndata;
+            delete [] r; 
+        }
+        for(auto i = back(); i != pos; i --)
+            * i = * (i - 1);
+        *pos = n;
+        return pos;
+    }
+
+    iterator erase( iterator pos )
+    {
+        if(length == 0)
+            exit(1);
+        length--;
+        for(auto i = pos; i < length; i ++)
+            *i = * (i + 1);
+        return pos;
+    }
+
+    void push_back( const T& n )
     {
         length++;
-        if(length==capacity_)
-        {
-            capacity_*=2;
-            pointer tmp=data_;
-            pointer ndata_=new value_type[capacity_];
-            for(size_type i=0;i<length;i++)
-                ndata_[i]=data_[i];
-            data_=ndata_;
-            delete[] tmp;
+        if(length > capacity){
+            T * r = _data;
+            capacity = capacity * 2;
+            T * ndata = new T [capacity];
+            for(size_t i = 0; i < length; i ++) 
+                ndata[i] = _data[i];
+            _data = ndata;
         }
-        for(auto i=end()-1;i!=pos;i--)
-            *i=*(i-1);
-        *pos=value;
-        return pos;
-    }    
+        _data[length] = n; 
+    }
+
+    void pop_back()
+    {
+        if(length > 0)
+            length--;
+    }
+
+    void resize(size_t count)
+    {
+        if(count < length)
+        {
+            length = count;
+            reserve(count * 2);
+        }
+        else {
+            reserve(count * 2);
+            length = count;
+        }
+    }
+
+    void resize( size_t count, const T & n )
+    {
+        if(count < length){
+            length = count;
+            reserve(count * 2);
+        }
+        else
+        {
+            reserve(count * 2);
+            for(size_t i = length; i < count; i ++)
+                _data[i] = n;
+            length = count;
+        }
+    }
+
+    void swap( vector & b )
+    {
+        T * t1 = _data;
+        _data = b._data;
+        b._data = t1;
+        delete [] t1;
+        size_t t2 = length;
+        length = b.length;
+        b.length = t2;
+        size_t t3 = _capacity;
+        _capacity = b._capacity;
+        b._capacity = t3;
+    }
 };
 #endif
